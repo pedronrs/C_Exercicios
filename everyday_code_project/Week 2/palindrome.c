29d247eea5175c7a8461ca961847f055981aaf85
pedronrs <pedro.reis.ismart@gmail.com>
2025-08-14 22:37:44 -0300

    Correção exercicio

--- everyday_code_project/Week 2/palindrome.c
+++ everyday_code_project/Week 2/palindrome.c
@@ -4,27 +4,30 @@ int palindromo_v( char* s);
 
 int main()
 {
+    int result;
     char palindromo[] = "ana";
     char naopalindromo[] = "sagradas";
     palindromo_v(palindromo);
-    palindromo_v(naopalindromo); 
+    palindromo_v(naopalindromo);
     return 0;
 }
 
 int palindromo_v(char* s)
 {
     char c;
-    int i,size=0;
+    int i =0;int size=0;
     while(s[size]){size++;}; //strlen
     int j = size -1;
-    while(i < j)
+    while(i < j) //laço comparando primeiro e ultimo digito
     {
         if(s[i] != s[j])
         {
-            return 0; // não é palíndromo
+            printf("%s não é palíndromo", s);
+            return 0; 
         }
-        i++;
+        i++; 
         j--;
     }
+    printf("%s é palíndromo", s);
     return 1;
 }
